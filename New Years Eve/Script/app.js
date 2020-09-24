4c38c3e8b77f010a72c2ba4677898bf6da4856e3
marquise <mdrharding30@gmail.com>
2020-09-24 15:51:20 -0600

    New Years Eve Update
    Want to practice in making another countdown timer

---
+++ New Years Eve/Script/app.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+const newYearEve = '31 Dec 2020'
+
+function countdown () {
+  const newYearEveDate = new Date(newYearEve)
+  const currentdate = new Date()
+
+  const totalSeconds = (newYearEveDate - currentdate) / 1000
+
+  const days = Math.floor(totalSeconds / 3600 / 24)
+  const hours = Math.floor(totalSeconds / 3600) % 24
+  const mins = Math.floor(totalSeconds / 60) % 60
+  const seconds = Math.floor(totalSeconds) % 60
+
+  daysEl.innerHTML = days
+  hoursEl.innerHTML = hours
+  minsEl.innerHTML = mins
+  secondsEl.innerHTML = seconds
+}
+function formatTime (time) {
+  return time < 10 ? `0${time}` : time
+}
+countdown()
+
+setInterval(countdown, 1000)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Countdown-timer
+
+I will make three countdown timers with vanilla javascript. At least the last one will use moment.js.
